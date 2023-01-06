371b6d180ef4ac87d7d7edd3b56e9bf4b356a049
Matthew Hyatt <mhyatt000@gmail.com>
2023-01-06 14:53:10 -0600

    deploy

--- listeners.js
+++ listeners.js
@@ -2,23 +2,37 @@ function getXpath(path) {
   return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
-function onHover(xpath) {
-  const element = getXpath(xpath)
-
+function hoverListener(element){
   element.addEventListener('mouseenter', () => {
-    element.style.transform = 'scale(1.2)';
+    element.style.transform = `scale(${ratio})`;
   });
   element.addEventListener('mouseleave', () => {
     element.style.transform = 'scale(1)';
   });
 }
 
-onHover('//i/../../a[1]/i')
-onHover('//i/../../a[2]/i')
-onHover('//i/../../a[3]/i')
-onHover('//i/../../a[4]/i')
-onHover('//i/../../a[5]/i')
+function onHover(xpath=null, alltag=null, ratio=1.1) {
+  if !(xpath || alltag)
+    return
 
-onHover('//nav/*[2]/*')
+  if (xpath) {
+      const element = getXpath(xpath)
+      hoverListener(element)
+  }
+
+  if (alltag){
+      const elements = document.getElementsByTagName(alltag)
+      for (const element in elements) {
+        hoverListener(element)
+      }
+}
+
+onHover('//i/../../a[1]/i', 1.2)
+onHover('//i/../../a[2]/i', 1.2)
+onHover('//i/../../a[3]/i', 1.2)
+onHover('//i/../../a[4]/i', 1.2)
+onHover('//i/../../a[5]/i', 1.2)
+
+onHover('//nav/*[2]/*', 1.2)
 
 onHover('//button[1]')
